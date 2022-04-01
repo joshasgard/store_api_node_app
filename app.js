74bfcd106d4077972db032f22b8baf786c31f825
joshasgard <odesanmiidowu@gmail.com>
2022-04-01 23:28:49 +0100

    feat: Add product request routes and controllers

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ require('dotenv').config()
 const express = require('express');
 const app =express();
 
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
 
 const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
@@ -17,6 +19,8 @@ app.get('/', (req,res)=>{
     res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
 })
 
+app.use('/api/v1/products', productsRouter)
+
 // products route
 
 app.use(notFoundMiddleware)
@@ -27,6 +31,7 @@ const port = process.env.PORT || 3000
 const start = async ()=> {
     try {
         // connectDB
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, console.log('Server is listening on port 3000....'))
     } catch (error) {
         console.log(error)
